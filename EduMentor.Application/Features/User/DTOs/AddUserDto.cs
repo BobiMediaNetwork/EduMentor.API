831512f206f4635a2eaed7dfdb0c3d05b44fdec9
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2025-12-18 12:38:16 +0200

    EDU-11 - add: DTO for Users

---
+++ EduMentor.Application/Features/User/DTOs/AddUserDto.cs
@@ -0,0 +1,20 @@
+﻿using EduMentor.Application.Common;
+
+namespace EduMentor.Application.Features.User.DTOs;
+
+public class AddUserDto
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Username { get; set; } = string.Empty;
+
+    public DateOnly DateOfBirth { get; set; }
+
+    public string Password { get; } = Utils.CreateUniquePasswordForNewUsers(50);
+
+    public Guid RoleId { get; set; } = Guid.NewGuid();
+}
