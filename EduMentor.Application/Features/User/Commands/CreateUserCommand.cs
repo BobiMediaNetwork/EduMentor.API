5b6317a1898b7a43b333ae5a828227e536b8219c
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2025-12-18 14:00:51 +0200

    EDU-8- add: email send when users is added

--- EduMentor.Application/Features/User/Commands/CreateUserCommand.cs
+++ EduMentor.Application/Features/User/Commands/CreateUserCommand.cs
@@ -1,8 +1,10 @@
 ﻿using Ardalis.Result;
 using AutoMapper;
 using EduMentor.Application.Features.User.DTOs;
+using EduMentor.Application.Interfaces.Email;
 using EduMentor.Application.Interfaces.Repositories;
 using EduMentor.Domain.Generic;
+using EduMentor.Domain.Model;
 using MediatR;
 
 namespace EduMentor.Application.Features.User.Commands;
@@ -15,6 +17,7 @@ public class CreateUserCommand : IRequest<Result<ResponseType<ReadUserDto>>>
 public sealed class UserCommandHandler(
     IUserRepository userRepository,
     IRoleRepository roleRepository,
+    IEmailService emailService,
     IMapper mapper)
     : IRequestHandler<CreateUserCommand, Result<ResponseType<ReadUserDto>>>
 {
@@ -52,6 +55,37 @@ public sealed class UserCommandHandler(
             {
                 IsSuccess = false,
                 Message = addUserResponse.Message,
+            });
+        }
+
+        try
+        {
+            //TODO: add password reset link generation
+            var resetLink = $"https://localhost:7226/api/user/password-rest/123";
+
+            const string subject = "Welcome to EduMentor! We are glad to have you here!";
+
+            var body =
+                $"""
+                 <p>Hello!</p>
+                 <p>Welcome to <strong>EduMentor</strong>! Your account has been created!</p>
+                 <p>To get started, please set your password by clicking <a href='{resetLink}'>here</a> or by copying and pasting the following link into your browser:</p>
+                 <p><a href='{resetLink}'>{resetLink}</a></p>
+                 <p><strong>This link will expire in 1 hour.</strong></p>
+                 <p>Once your password is set, you can log in and start using EduMentor.</p>
+                 <p>We're excited to have you on board!</p>
+                 <p>Cheers,<br>The EduMentor Team</p>
+                 """;
+
+            await emailService.SendEmailAsync(addUserResponse.Object!.Email, subject, body);
+        }
+        catch (Exception ex)
+        {
+            return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+            {
+                IsSuccess = false,
+                Object = null,
+                Message = ex.Message
             });
         }
 
