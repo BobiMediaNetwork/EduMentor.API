49933c9eac8d1a82eee3214bed7e4d401fc47062
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2025-12-18 11:59:59 +0200

    EDU-12 - add: validator behavior

---
+++ EduMentor.Application/Common/ValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace EduMentor.Application.Common;
+
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators = validators;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+            return await next(cancellationToken);
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(
+            _validators.Select(v => v.ValidateAsync(context, cancellationToken))
+        );
+
+        var failures = validationResults
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null)
+            .ToList();
+
+        //TODO: Change to return something in the right format for the API
+        if (failures.Count != 0)
+            throw new ValidationException(failures);
+
+        return await next(cancellationToken);
+    }
+}
