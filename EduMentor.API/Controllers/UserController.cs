573940319f98553b65874bf97dbdc6d91deb62e0
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2025-12-18 13:04:51 +0200

    EDU-11 - add: controller for user CRUD

---
+++ EduMentor.API/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using EduMentor.Application.Features.User.Commands;
+using EduMentor.Application.Features.User.DTOs;
+using EduMentor.Application.Features.User.Queries;
+using EduMentor.Domain.Enum;
+using EduMentor.Domain.Generic;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EduMentor.API.Controllers;
+
+public class UserController(ISender sender) : BaseAPIController
+{
+    [HttpGet]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ResponseType<ReadUserDto>> GetAllUsers()
+    {
+        return await sender.Send(new GetAllUsersQuery());
+    }
+
+    [HttpGet("{userId:guid}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ResponseType<ReadUserDto>> GetUserById(Guid userId)
+    {
+        return await sender.Send(new GetUserByIdQuery { UserId = userId });
+    }
+
+    [HttpPost]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ResponseType<ReadUserDto>> AddUser(AddUserDto user)
+    {
+        return await sender.Send(new CreateUserCommand { User = user });
+    }
+
+    [HttpPut]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ResponseType<ReadUserDto>> UpdateUser(UpdateUserDto user)
+    {
+        return await sender.Send(new UpdateUserCommand { User = user });
+    }
+
+    [HttpDelete("{userId:guid}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ResponseType<bool>> DeleteUser(Guid userId)
+    {
+        return await sender.Send(new DeleteUserCommand { UserId = userId });
+    }
+
+    [HttpGet("all/{role}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<ResponseType<ReadUserDto>> GetAllUserByRole(RoleEnum role)
+    {
+        return await sender.Send(new GetAllUsersByRoleQuery { Role = role });
+    }
+}
