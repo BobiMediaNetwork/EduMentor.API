ed72f55e112dcd1379c5001494536053a2f062e7
Bolba-Mateescu Andrei <andrei_bolba@yahoo.com>
2025-12-18 12:56:45 +0200

    EDU-11 - add: command for users

---
+++ EduMentor.Application/Features/User/Commands/CreateUserCommand.cs
@@ -0,0 +1,65 @@
+﻿using Ardalis.Result;
+using AutoMapper;
+using EduMentor.Application.Features.User.DTOs;
+using EduMentor.Application.Interfaces.Repositories;
+using EduMentor.Domain.Generic;
+using MediatR;
+
+namespace EduMentor.Application.Features.User.Commands;
+
+public class CreateUserCommand : IRequest<Result<ResponseType<ReadUserDto>>>
+{
+    public required AddUserDto User { get; set; }
+}
+
+public sealed class UserCommandHandler(
+    IUserRepository userRepository,
+    IRoleRepository roleRepository,
+    IMapper mapper)
+    : IRequestHandler<CreateUserCommand, Result<ResponseType<ReadUserDto>>>
+{
+    public async Task<Result<ResponseType<ReadUserDto>>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var role = roleRepository.GetObjectById(request.User.RoleId);
+
+        if (role is { IsSuccess: false, Object: null })
+        {
+            return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+            {
+                IsSuccess = false,
+                Message = role.Message,
+            });
+        }
+
+        var isEmailAndUsernameUnique = userRepository.IsEmailAndUsernameUnique(request.User.Email, request.User.Username);
+
+        if (isEmailAndUsernameUnique is { IsSuccess: false, Object: null })
+        {
+            return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+            {
+                IsSuccess = false,
+                Message = isEmailAndUsernameUnique.Message,
+            });
+        }
+
+        var addUser = mapper.Map<Domain.Model.User>(request.User);
+
+        var addUserResponse = userRepository.Add(addUser);
+
+        if (addUserResponse is { IsSuccess: false, Object: null })
+        {
+            return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+            {
+                IsSuccess = false,
+                Message = addUserResponse.Message,
+            });
+        }
+
+        return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+        {
+            IsSuccess = true,
+            Message = "User added successfully!",
+            Object = mapper.Map<ReadUserDto>(addUserResponse.Object!),
+        });
+    }
+}
---
+++ EduMentor.Application/Features/User/Commands/UpdateUserCommand.cs
@@ -0,0 +1,78 @@
+﻿using Ardalis.Result;
+using AutoMapper;
+using EduMentor.Application.Features.User.DTOs;
+using EduMentor.Application.Interfaces.Repositories;
+using EduMentor.Domain.Generic;
+using MediatR;
+
+namespace EduMentor.Application.Features.User.Commands;
+
+public class UpdateUserCommand : IRequest<Result<ResponseType<ReadUserDto>>>
+{
+    public required UpdateUserDto User { get; set; }
+}
+
+public sealed class UpdateUserCommandHandler(
+    IUserRepository userRepository, IRoleRepository roleRepository, IMapper mapper)
+    : IRequestHandler<UpdateUserCommand, Result<ResponseType<ReadUserDto>>>
+{
+    public async Task<Result<ResponseType<ReadUserDto>>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        if (request.User.RoleId.HasValue)
+        {
+            var role = roleRepository.GetObjectById(request.User.RoleId.Value);
+
+            if (role is { IsSuccess: false, Object: null })
+            {
+                return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+                {
+                    IsSuccess = false,
+                    Message = role.Message,
+                });
+            }
+        }
+
+        var isEmailAndUsernameUnique = userRepository.IsEmailAndUsernameUnique(request.User.Email, request.User.Username);
+
+        if (isEmailAndUsernameUnique is { IsSuccess: false, Object: null })
+        {
+            return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+            {
+                IsSuccess = false,
+                Message = isEmailAndUsernameUnique.Message,
+            });
+        }
+
+        var user = userRepository.GetUserByIdWithOwnProperties(request.User.Id);
+
+        if (user is { IsSuccess: false, Object: null })
+        {
+            return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+            {
+                IsSuccess = false,
+                Message = user.Message
+            });
+        }
+
+        request.User.DateOfBirth ??= user.Object!.DateOfBirth;
+        mapper.Map(request.User, user.Object);
+
+        var updateResponse = userRepository.Update(user.Object!);
+
+        if (updateResponse is { IsSuccess: false, Object: null })
+        {
+            return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+            {
+                IsSuccess = false,
+                Message = updateResponse.Message,
+            });
+        }
+
+        return Result<ResponseType<ReadUserDto>>.Success(new ResponseType<ReadUserDto>
+        {
+            IsSuccess = true,
+            Message = "User updated successfully!",
+            Object = mapper.Map<ReadUserDto>(updateResponse.Object!),
+        });
+    }
+}
